7f5983522122a192671d272bb0ce4e65ee79c81b
배성진 <tohinata@naver.com>
2024-01-15 23:21:20 +0900

    [Update] 5. Character Movement to Left and Right
    - 마우스 클릭/드래그로 캐릭터 좌우 이동 (X 좌표)
    - Floor 안 떨어지게 처리

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float playerSpeed = 5f;
+    float movingSiesSpeed = 0.2f;
+    Vector3 firstPos;
+    Vector3 lastPos;
+    float maxXPosition = 4.35f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, playerSpeed * Time.deltaTime);
+        float xValue = 0;
+        if (Input.GetMouseButtonDown(0))
+		{
+            firstPos = Input.mousePosition;
+		}
+        else if (Input.GetMouseButton(0))
+		{
+            lastPos = Input.mousePosition;
+            float differences = lastPos.x - firstPos.x;
+            xValue = differences * movingSiesSpeed;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+		{
+            firstPos = Vector3.zero;
+            lastPos = Vector3.zero;
+            xValue = 0;
+		}
+        
+        transform.Translate(xValue * Time.deltaTime, 0, playerSpeed * Time.deltaTime);
+
+        float newXValue = Mathf.Clamp(transform.position.x, -maxXPosition, maxXPosition);
+        transform.position = new Vector3(newXValue, transform.position.y, transform.position.z);
     }
 }
